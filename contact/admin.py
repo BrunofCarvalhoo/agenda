1c36df5e7c800716dd8f7b201d449c879aabba45
Bruno <bcf2@cesar.school>
2025-08-25 16:11:32 -0300

    ajustando o admin

--- contact/admin.py
+++ contact/admin.py
@@ -3,4 +3,11 @@ from contact import models
 # Register your models here.
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...        
+    list_display = 'id','first_name','last_name','phone', # no Django admin vai aparecer esses campos
+    ordering = '-id', # ordenar por id de forma decrescente
+    #list_filter = 'created_date',
+    search_fields = 'id','first_name','last_name', # serve para fazer buscas
+    list_per_page = 10 # exibir apenas 10 contatos por pagina
+    list_max_show_all = 200 # so vai aparecer o 'mostrar tudo' se tiver com menos de 200 contatos
+    list_editable = 'first_name','last_name', # esses campos podem ser editados diretamente na listagem
+    list_display_links = 'id', 'phone', # esses campos vão ser links para editar o contato
