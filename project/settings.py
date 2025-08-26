5ba3e57b648fb0bd18e6463737d84d61880cffad
Bruno <bcf2@cesar.school>
2025-08-26 17:10:47 -0300

    update settigns

--- project/settings.py
+++ project/settings.py
@@ -130,3 +130,9 @@ MEDIA_ROOT = BASE_DIR / 'media' # local onde os arquivos enviados serao armazena
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from project.local_settings import *
+except ImportError:
+    ...
