eeaac8e6973e22f3c0dc7020a3666b8518b92838
Bruno <bcf2@cesar.school>
2025-09-03 19:44:01 -0300

    contato unico

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_list_or_404
 from contact.models import Contact
+#from django.http import Http404
 # Create your views here.
 def index(request):
 
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(show=True).order_by('-id')[:10] # seleciona os contatos onde o show é True
     context = {
         'contacts': contacts
     }
@@ -12,3 +13,16 @@ def index(request):
         'contact/index.html',
         context
     )
+
+def contact(request, contact_id):
+    
+    single_contact = get_list_or_404(Contact,pk=contact_id, show=True) 
+           
+    context = {
+        'contact': single_contact
+    }
+    return render(
+        request,
+        'contact/contact.html',
+        context
+    ) 
