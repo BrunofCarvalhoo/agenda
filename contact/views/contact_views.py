c6129ea0cdc3ce2271957e59bfec367190858428
Bruno <bcf2@cesar.school>
2025-09-04 18:03:44 -0300

    update

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -6,7 +6,8 @@ def index(request):
 
     contacts = Contact.objects.filter(show=True).order_by('-id')[:10] # seleciona os contatos onde o show é True
     context = {
-        'contacts': contacts
+        'contacts': contacts,
+        'site_title': 'Contatos - '
     }
     return render(
         request,
@@ -18,8 +19,11 @@ def contact(request, contact_id):
 
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
 
+    site_title = f'{single_contact.first_name} {single_contact.last_name} -'
+
     context = {
-        'contact': single_contact
+        'contact': single_contact,
+        'site_title': f'Contato  {site_title}'
     }
     return render(
         request,
