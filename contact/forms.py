64b42d82f011d94f64f41ad37b74c490de384b22
Bruno <bcf2@cesar.school>
2025-09-23 18:03:14 -0300

    create e update

--- contact/forms.py
+++ contact/forms.py
@@ -27,33 +27,40 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = models.Contact
         fields = (
-            'first_name', 'last_name', 'phone',
+            'first_name', 'last_name', 'phone', 'email', 'description','category',
         )
         # widgets = {
         #     'first_name': forms.TextInput(
         #         attrs={
         #             'class': 'classe-a classe-b',
-        #             'placeholder': 'Escreva aqui',
+        #             'placeholder': 'digite aqui',
         #         }
         #     )
         # }
 
     def clean(self):
-        # cleaned_data = self.cleaned_data
+        cleaned_data = self.cleaned_data
 
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+        if first_name == last_name:
+            msg = ValidationError(
+                'Primeiro nome nao pode ser igual ao segundo',
                 code='invalid'
             )
-        )
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
 
         return super().clean()
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'veio do add_error',
+                    code='invalid'
+                )
+            )
+        return first_name
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,15 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from contact.forms import ContactForm
-
-
+from django.urls import reverse
+from contact.models import Contact
 def create(request):
+    form_action = reverse('contact:create')
     if request.method == 'POST':
-        
+        form = ContactForm(request.POST)
         context = {
-            'form': ContactForm(request.POST)
+            'form': form,
+            'form_action': form_action,
         }
 
-
+        if form.is_valid(): # retorna True ou False , retorna true se o form nao tem nenhum erro
+            print('formulario é valido')
+            # form.save()
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.pk) # pode ser id ou pk nesse caso
+ 
         return render(
             request, 
             'contact/create.html',
@@ -17,7 +24,42 @@ def create(request):
         )
     
     context = {
-            'form': ContactForm()
+            'form': ContactForm(),
+            'form_action': form_action,
+        }
+    return render(
+            request,
+            'contact/create.html',
+            context
+        )
+    
+def update(request,contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+
+    form_action = reverse('contact:update',args=(contact_id,))
+    
+    if request.method == 'POST':
+        form = ContactForm(request.POST, instance=contact)
+        context = {
+            'form': form,
+            'form_action': form_action,
+        }
+
+        if form.is_valid(): # retorna True ou False , retorna true se o form nao tem nenhum erro
+            print('formulario é valido')
+            # form.save()
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.pk) # pode ser id ou pk nesse caso
+ 
+        return render(
+            request, 
+            'contact/create.html',
+            context
+        )
+    
+    context = {
+            'form': ContactForm(isinstance=contact),
+            'form_action': form_action,
         }
     return render(
             request,
