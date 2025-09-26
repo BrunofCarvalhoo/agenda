b39fb023e9a3c352f29d171d104b3d4bbbb50412
Bruno <bcf2@cesar.school>
2025-09-26 18:28:00 -0300

    update

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from contact.forms import ContactForm
 from django.urls import reverse
 from contact.models import Contact
+from django.contrib.auth.decorators import login_required
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
     if request.method == 'POST':
@@ -14,7 +16,9 @@ def create(request):
         if form.is_valid(): # retorna True ou False , retorna true se o form nao tem nenhum erro
             print('formulario é valido')
             # form.save()
-            contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner  = request.user
+            contact.save()
             return redirect('contact:update', contact_id=contact.pk) # pode ser id ou pk nesse caso
  
         return render(
@@ -32,9 +36,9 @@ def create(request):
             'contact/create.html',
             context
         )
-    
+@login_required(login_url='contact:login')
 def update(request,contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(Contact, pk=contact_id, show=True,owner=request.user)
 
     form_action = reverse('contact:update',args=(contact_id,))
     
@@ -66,9 +70,9 @@ def update(request,contact_id):
             'contact/create.html',
             context
         )
-
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(Contact, pk=contact_id, show=True,owner=request.user)
     confirmacao = request.POST.get('confirmacao','no')
 
     if confirmacao == 'yes':
